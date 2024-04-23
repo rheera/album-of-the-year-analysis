cc37178d661ebaddfb74963ac563e9c71c3f3336
rheera <rheera2@uwo.ca>
2024-04-23 16:28:19 -0400

    add docstrings to functions

--- compare_combos.py
+++ compare_combos.py
@@ -27,14 +27,46 @@ df["genres"] = df["genres"].apply(literal_eval)
 
 
 def get_genre_df(genre):
+    """
+    Gets a DataFrame of album information of albums that contain the genre passed.
+        Parameters:
+                genre (str): The genre to filter by
+
+        Returns:
+                filtered dataframe (DataFrame):
+    """
     return df[df["genres"].apply(lambda x: genre in x)]
 
 
 def genre_total(genre):
+    """
+    Returns the total number of albums that include a certain genre
+        Parameters:
+                genre (str): The genre to count
+
+        Returns:
+                total albums (int):
+    """
     return len(get_genre_df(genre).index)
 
 
 def genre_releases_by_year(genre, start_year=1955, end_year=2024):
+    """
+    Returns a DataFrame of a certain genres album releases grouped by release year
+        Parameters
+        ----------
+        genre : str
+            The genre you want to filter by
+        start_year : int, optional
+            The first year you want to gather albums from , inclusive (default is 1955)
+        last_year : int, optional
+            The last year you want to gather albums from, exclusive (default is 2024)
+
+        Returns
+        -------
+        grouped_dataframe: DataFrame
+            A DataFrame grouping the album releases by year by genre
+    """
     genre_df = get_genre_df(genre)
     genre_filtered_df = genre_df[
         (genre_df["list_year"] >= start_year) & (genre_df["list_year"] <= end_year)
@@ -43,7 +75,21 @@ def genre_releases_by_year(genre, start_year=1955, end_year=2024):
 
 
 def compare_genres_releases_by_year(*args, start_year=1955, end_year=2024):
+    """
+    Returns a plotly line graph comparing the number of album releases a genre by year.
+        Parameters
+        ----------
+        args : str
+            The different genres you want to compare. Can be unlimited but the line graph might become messy
+        start_year : int, optional
+            The first year you want to gather albums from , inclusive (default is 1955)
+        last_year : int, optional
+            The last year you want to gather albums from, exclusive (default is 2024)
 
+        Returns
+        -------
+        A plotly line graph
+    """
     genre_dfs = []
     for genre in args:
         genre_dfs.append(genre_releases_by_year(genre, start_year, end_year))
@@ -71,6 +117,24 @@ compare_genres_releases_by_year(
 
 
 def get_genre_top_common_genres(genre, threshold=10, start_year=1955, end_year=2024):
+    """
+    Returns a DataFrame of a certain genres common genres.
+        Parameters
+        ----------
+        genre : str
+            The genre you want to filter by
+        threshold : int, optional
+            The amount of genres to return in the dataframe (default is 10)
+        start_year : int, optional
+            The first year you want to gather albums from , inclusive (default is 1955)
+        last_year : int, optional
+            The last year you want to gather albums from, exclusive (default is 2024)
+
+        Returns
+        -------
+        top_genres_dataframe: DataFrame
+            A DataFrame with the top occuring genres of a certain genre.
+    """
     df = get_genre_df(genre)
     genre_filtered_df = df[
         (df["list_year"] >= start_year) & (df["list_year"] <= end_year)
@@ -83,6 +147,23 @@ def get_genre_top_common_genres(genre, threshold=10, start_year=1955, end_year=2
 
 
 def compare_genres_occurences(*args, threshold=10, start_year=1955, end_year=2024):
+    """
+    Returns multiple Seaborn heatmaps representing a genre and the amount of times other genres show up alongside it on albums.
+        Parameters
+        ----------
+        args : str
+            The different genres you want to compare. Can be unlimited but the amount of graphs can be messy.
+        threshold : int, optional
+            The amount of genres you want to show on the heatmap (default is 10)
+        start_year : int, optional
+            The first year you want to gather albums from , inclusive (default is 1955)
+        last_year : int, optional
+            The last year you want to gather albums from, exclusive (default is 2024)
+
+        Returns
+        -------
+        A Seaborn figure with multiple heatmaps of the different genres.
+    """
     top_1 = get_genre_top_common_genres(args[0], threshold, start_year, end_year)
     num_of_genres = len(args)
     print(num_of_genres)
